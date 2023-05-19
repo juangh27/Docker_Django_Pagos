e19eb50da1e13f559fc80d3977a8d84abf23c562
juangh27 <101841901+juangh27@users.noreply.github.com>
2023-05-18 21:51:49 -0600

    test

--- server/nido/urls.py
+++ server/nido/urls.py
@@ -37,12 +37,4 @@ urlpatterns = [
     
     
 ]
-from django.urls import path
 
-from . import views
-
-urlpatterns = [
-    path('', views.test, name='test'),
-    path('test2/', views.test2, name='test2'),
-    
-]
